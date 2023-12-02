4561c80bd83abe75e361c45380b21244458e7973
HoangNguyen <huyhoang25041999@gmail.com>
2023-12-02 15:49:15 +0700

    Init project trello

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// import { useState } from "react";
+import "./App.css";
+
+function App() {
+  // const [age, setAge] = useState();
+  return (
+    <>
+      <div id="container"></div>
+    </>
+  );
+}
+
+export default App;
